416b942c4d25eb97a56f4eb1168e43232be3fcdb
Mirmillon <xavier.compagnion@gmail.com>
2020-05-14 13:27:44 +0800

    Premier commit

---
+++ Models/PeopleDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleManagement.Models;
+using PeopleManagement.Models.Telecommunications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleManagement.API.Models
+{
+    public class PeopleDbContext :DbContext
+    {
+        public PeopleDbContext(DbContextOptions<PeopleDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Code to seed data
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Mobile> Mobiles { get; set; }
+        public DbSet<Landline> Landlines { get; set; }
+    }
+}
